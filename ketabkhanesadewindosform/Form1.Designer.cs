07cbb37b278b056647722da71b73aad27dfb4ad9
fati <fatihashemi721fh@gmail.com>
2025-10-17 09:05:23 +0330

    اولین نسخه

---
+++ ketabkhanesadewindosform/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ketabkhanesadewindosform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(489, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "برای نمایش فشار دهید";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(615, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "لیست کتاب ها";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "لیست اعضا";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(489, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "برای نمایش فشار دهید";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ ketabkhanesadewindosform/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ketabkhanesadewindosform
+{
+
+   
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Book x = new Book( "بوف کور","صادق هدایت","978-964-448-008-5","1385");
+            Book y = new Book("ملت عشق", "ارسلان فصیحی", "978-600-8132-41-3", "1395");
+            Book u = new Book("بی کتاب", "محمدرضاشرفی خبوشان", "878-600-175-631-9", "1395");
+            MessageBox.Show(x.print() + "\n" + y.print() + "\n" + u.print()); 
+        }
+        public class Book
+        {
+
+
+            /// peroperti/////
+
+
+            private string Title
+            {
+                get;
+                set;
+            }
+            private string Author
+            {
+                get;
+                set;
+            }
+            private string Isbn
+            {
+                get;
+                set;
+            }
+            private string YrearOfPblication
+            {
+                get;
+                set;
+            }
+
+            ////////constractor////
+            public Book(string Title, string Author, string Isbn, string YrearOfPblication)
+            {
+                this.Title = Title;
+                this.Author = Author;
+                this.Isbn = Isbn;
+                this.YrearOfPblication = YrearOfPblication;
+            }
+            //////metood//////
+            public string print()
+            {
+                string n = Title + ",   " + Author + ",   " + Isbn + ",  " + YrearOfPblication;
+                return n;
+             
+            }
+
+        }
+        public class Member
+        {
+            /////peroperti/////
+            private string Name
+            {
+                get;
+                set;
+            }
+            private string MemberId
+            {
+                get;
+                set;
+            }
+            private string Phone
+            {
+                get;
+                set;
+            }
+
+            ////////constractor////
+            public Member(string Name, string MemberId, string Phone)
+            {
+                this.Name = Name;
+                this.MemberId = MemberId;
+                this.Phone = Phone;
+            }
+            ////metood///
+            public string printmemberinfo()
+            {
+                string m = Name + ",   " + MemberId + ",   " + Phone;
+                return m;
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Member a = new Member("فاطی", "fatiii", "099100000");
+            MessageBox.Show(a.printmemberinfo());
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
